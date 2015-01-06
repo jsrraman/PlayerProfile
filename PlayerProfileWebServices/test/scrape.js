972f83686cd4788413673d9348caf0b90b024228
jsrraman <rajaraman.ks@gmail.com>
2015-01-06 08:00:50 +0530

    Updated mocha test cases description

--- PlayerProfileWebServices/test/scrape.js
+++ PlayerProfileWebServices/test/scrape.js
@@ -11,7 +11,7 @@ describe('Player Data Scrape Unit Test Report', function() {
     // This is going to take a while so set a longer timeout (10 seconds)
     this.timeout(10*1000);
 
-    it('should store the list of cricket playing countries successfully', function(done) {
+    it('should scrape and store the list of cricket playing countries successfully', function(done) {
         request(url)
             .get('/scrape/countries')
             .expect(200)
@@ -25,7 +25,7 @@ describe('Player Data Scrape Unit Test Report', function() {
             });
     });
 
-    it("should store the player list for England successfully", function(done) {
+    it("should scrape and store the player list for England successfully", function(done) {
         request(url)
             .get("/scrape/players/country?countryId=1&name=England")
             .expect(200)
@@ -39,7 +39,7 @@ describe('Player Data Scrape Unit Test Report', function() {
             });
     });
 
-    it("should store the list of all players playing for USA successfully", function(done) {
+    it("should scrape and store the player list for USA successfully", function(done) {
 
         request(url)
             .get("/scrape/players/country?countryId=11&name=USA")
@@ -54,7 +54,7 @@ describe('Player Data Scrape Unit Test Report', function() {
             });
     });
 
-    it("should store the player list for player id = 8917 successfully", function (done) {
+    it("should scrape and store the player list for player id = 8917 successfully", function (done) {
         request(url)
             //.get("/scrape/player?countryId=1&playerId=8917&url=http://www.espncricinfo.com/england/content/player/8917.html")
             .get("/scrape/player?playerId=8917")
